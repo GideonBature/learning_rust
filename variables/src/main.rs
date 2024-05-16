5c563e889029ec946bf128e80d5c845c044724fb
GideonBature <infoaboutgideon@gmail.com>
2024-05-16 11:08:46 +0100

    integer datatypes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,26 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    println!("The value of 3 hours in seconds is: {THREE_HOURS_IN_SECONDS}");
+
+    let a = 5;
+    let a = a + 1;
+    {
+        let a = a * 2;
+        println!("The value of a in the inner scope is: {a}");
+    }
+    println!("The value of a is: {a}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("The value of spaces is: {spaces}");
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    println!("The value of guess is: {guess}");
 }
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/learning_rust/variables/target/debug/variables: /home/bene/Desktop/learning_rust/variables/src/main.rs
