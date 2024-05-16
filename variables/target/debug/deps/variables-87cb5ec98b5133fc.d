9ea6bef543ea20f0406260d3124dea3dce9e3b4a
GideonBature <infoaboutgideon@gmail.com>
2024-05-16 03:50:27 +0100

    testing immutability of variables

---
+++ variables/target/debug/deps/variables-87cb5ec98b5133fc.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/learning_rust/variables/target/debug/deps/variables-87cb5ec98b5133fc: src/main.rs
+
+/home/bene/Desktop/learning_rust/variables/target/debug/deps/variables-87cb5ec98b5133fc.d: src/main.rs
+
+src/main.rs:
